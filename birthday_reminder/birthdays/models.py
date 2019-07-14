f18225fdbf0ccd89873fb42ebc73003e10164343
ykuruta <yasukokurata@gmail.com>
2019-07-14 16:27:17 -0400

    count age

--- birthday_reminder/birthdays/models.py
+++ birthday_reminder/birthdays/models.py
@@ -1,12 +1,16 @@
 from django.conf import settings
 from django.db import models
-from djangoyearlessdate.models import YearlessDateField
+import datetime
 
 
 class Birthday(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, help_text="yyyy-mm-dd")
-    
+
+    @property
+    def age(self):
+        return int((datetime.datetime.now().date() - self.date_of_birth).days / 365.25 )
+
     def __str__(self):
         return self.name
